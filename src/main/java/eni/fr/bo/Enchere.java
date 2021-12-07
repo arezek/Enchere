ea87d83e8f369df5aa6c86a5f6714afd8cd8ee28
eugen <eugen@DESKTOP-5J8KJRS>
2021-12-07 09:03:02 +0100

    Enchere version 6 dec

---
+++ src/main/java/eni/fr/bo/Enchere.java
@@ -0,0 +1,43 @@
+package eni.fr.bo;
+
+import java.time.LocalDate;
+
+public class Enchere {
+	
+
+	LocalDate dateEnchere;
+	int montantEnchere;
+	
+	public Enchere() {
+	}
+
+	public Enchere(LocalDate dateEnchere, int montantEnchere) {
+		super();
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+	}
+
+	public LocalDate getDateEnchere() {
+		return dateEnchere;
+	}
+
+	public void setDateEnchere(LocalDate dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	public int getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	public void setMontantEnchere(int montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+
+	@Override
+	public String toString() {
+		return "Enchere [dateEnchere=" + dateEnchere + ", montantEnchere=" + montantEnchere + "]";
+	}
+	
+	
+
+}
