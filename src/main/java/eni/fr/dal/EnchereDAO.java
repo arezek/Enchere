23049267baa73260e87a0c0bda59ec68f1f55536
eugen <eugen@DESKTOP-5J8KJRS>
2021-12-07 09:25:03 +0100

    DAO bien fait

--- src/main/java/eni/fr/dal/EnchereDAO.java
+++ src/main/java/eni/fr/dal/EnchereDAO.java
@@ -1,26 +1,27 @@
 package eni.fr.dal;
 
-import eni.fr.bo.Categorie;
+import eni.fr.bo.Enchere;
 
 /**
  * @author EUGENIE FUCHS 
  */
 
-public class EnchereDAO {
+public interface EnchereDAO {
 	
 	//Sélectionner un Categorie par son noCategorie
 	public Enchere selectById(int noCategorie) throws DALException;
-			
+				
 	//Sélectionner tous les Categories 
 	public Enchere selectAll() throws DALException;
-			
+				
 	//Modifier les attributs d'un Categorie connu en BD
 	public void update(Enchere enchere) throws DALException;
-			
+				
 	//Insérer une nouvelle Categorie
 	public void insert(Enchere enchere) throws DALException;
-			
+				
 	//Supprimer une Categorie
 	public void delete(int id) throws DALException;
+
 
 }
