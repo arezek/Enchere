f2fab73f123fce97bf6c98aed6fdf53bcc5c2d36
Morrizzio <fabien.gavoille@hotmail.fr>
2021-12-13 11:12:59 +0100

    modif recherche partielle

--- src/main/java/eni/fr/dal/ArticleVenduDAOJdbcImpl.java
+++ src/main/java/eni/fr/dal/ArticleVenduDAOJdbcImpl.java
@@ -19,7 +19,8 @@ import eni.fr.bo.Utilisateur;
 public class ArticleVenduDAOJdbcImpl implements ArticleVenduDAO {
 
 	private static final String INSERT = "INSERT INTO ARTICLES_VENDUS(nom_article,description , date_debut_encheres, date_fin_encheres, prix_initial, etat_vente,no_utilisateur,no_categorie ) VALUES(?,?,?,?,?,?,?,2)";
-	private static final String SEARCH = "SELECT no_article, nom_article,description,date_debut_encheres,date_fin_encheres, prix_initial,etat_vente, ARTICLES_VENDUS.no_utilisateur,ARTICLES_VENDUS.no_categorie, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur, libelle FROM ARTICLES_VENDUS  INNER JOIN UTILISATEURS ON UTILISATEURS.no_utilisateur = ARTICLES_VENDUS.no_utilisateur inner join CATEGORIES on CATEGORIES.no_categorie=ARTICLES_VENDUS.no_categorie WHERE nom_article =? AND ARTICLES_VENDUS.no_categorie =?";
+	private static final String SEARCH = "SELECT no_article, nom_article,description,date_debut_encheres,date_fin_encheres, prix_initial,etat_vente, ARTICLES_VENDUS.no_utilisateur,ARTICLES_VENDUS.no_categorie, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur, libelle FROM ARTICLES_VENDUS  INNER JOIN UTILISATEURS ON UTILISATEURS.no_utilisateur = ARTICLES_VENDUS.no_utilisateur inner join CATEGORIES on CATEGORIES.no_categorie=ARTICLES_VENDUS.no_categorie WHERE nom_article like ? AND ARTICLES_VENDUS.no_categorie =?";
+	private static final String SEARCHNOCATEGORIE = "SELECT no_article, nom_article,description,date_debut_encheres,date_fin_encheres, prix_initial,etat_vente, ARTICLES_VENDUS.no_utilisateur,ARTICLES_VENDUS.no_categorie, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur, libelle FROM ARTICLES_VENDUS  INNER JOIN UTILISATEURS ON UTILISATEURS.no_utilisateur = ARTICLES_VENDUS.no_utilisateur inner join CATEGORIES on CATEGORIES.no_categorie=ARTICLES_VENDUS.no_categorie WHERE nom_article like ?";
 	private static final String SELECTBYID = "SELECT no_article, nom_article,description,date_debut_encheres,date_fin_encheres, prix_initial,etat_vente, ARTICLES_VENDUS.no_utilisateur,ARTICLES_VENDUS.no_categorie, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur, libelle FROM ARTICLES_VENDUS  INNER JOIN UTILISATEURS ON UTILISATEURS.no_utilisateur = ARTICLES_VENDUS.no_utilisateur inner join CATEGORIES on CATEGORIES.no_categorie=ARTICLES_VENDUS.no_categorie WHERE ARTICLES_VENDUS.no_article =?";
 	private static final String SELECTALL = "SELECT no_article, nom_article,description,date_debut_encheres,date_fin_encheres, prix_initial,etat_vente, ARTICLES_VENDUS.no_utilisateur,ARTICLES_VENDUS.no_categorie, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur, libelle FROM ARTICLES_VENDUS  INNER JOIN UTILISATEURS ON UTILISATEURS.no_utilisateur = ARTICLES_VENDUS.no_utilisateur inner join CATEGORIES on CATEGORIES.no_categorie=ARTICLES_VENDUS.no_categorie ORDER BY date_fin_encheres DESC";
 	private static final String DELETE = "delete from ARTICLES_VENDUS where no_article=?";
@@ -197,12 +198,48 @@ public class ArticleVenduDAOJdbcImpl implements ArticleVenduDAO {
 
 		ResultSet rs = null;
 		List<ArticleVendu> liste = new ArrayList<ArticleVendu>();
+		if (noCategorie == 5) {
+			try (Connection con = ConnectionProvider.getConnection(); 
+					 PreparedStatement rqt = con.prepareStatement(SEARCHNOCATEGORIE);)
+
+				{
+					ArticleVendu art = new ArticleVendu();
+					rqt.setString(1, "%"+nomArticle+"%");
+					
+					rs = rqt.executeQuery();
+//					rs.next();
+
+					
+
+					while (rs.next()) {
+						Utilisateur utilisateur = new Utilisateur(rs.getInt("no_utilisateur"), rs.getString("pseudo"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("telephone"),
+								rs.getString("rue"), rs.getString("code_postal"), rs.getString("ville"), rs.getString("mot_de_passe"), rs.getInt("credit"), rs.getBoolean("administrateur"));
+						
+						Categorie categorie = new Categorie(rs.getInt("no_categorie"), rs.getString("libelle"));
+						
+						art = new ArticleVendu(rs.getInt("no_article"), rs.getString("nom_article"),rs.getString("description"),rs.getDate("date_debut_encheres").toLocalDate(),
+								rs.getDate("date_fin_encheres").toLocalDate(), rs.getInt("prix_initial"), rs.getString("etat_vente"));
+						
+						art.setNoUtilisateur(utilisateur);
+						art.setNoCategorie(categorie);
+						
+						liste.add(art);
+						
+					}
+
+						
+
+				} catch (SQLException e) {
+					
+					throw new DALException("erreur de requete de recherche d'articles", e);
+				}
+		}else {
 		try (Connection con = ConnectionProvider.getConnection(); 
 			 PreparedStatement rqt = con.prepareStatement(SEARCH);)
 
 		{
 			ArticleVendu art = new ArticleVendu();
-			rqt.setString(1, nomArticle);
+			rqt.setString(1, "%"+nomArticle+"%");
 			rqt.setInt(2, noCategorie);
 			rs = rqt.executeQuery();
 //			rs.next();
@@ -230,6 +267,7 @@ public class ArticleVenduDAOJdbcImpl implements ArticleVenduDAO {
 		} catch (SQLException e) {
 			
 			throw new DALException("erreur de requete de recherche d'articles", e);
+		}
 		}
 
 		return liste;
