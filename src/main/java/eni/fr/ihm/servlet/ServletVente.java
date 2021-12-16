359f365f6fd044d304b147656eaf12667204b729
eugen <eugen@DESKTOP-5J8KJRS>
2021-12-16 12:15:44 +0100

    servlet vente sans warning et auteurs à jour

--- src/main/java/eni/fr/ihm/servlet/ServletVente.java
+++ src/main/java/eni/fr/ihm/servlet/ServletVente.java
@@ -1,9 +1,7 @@
 package eni.fr.ihm.servlet;
 
-import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -13,146 +11,143 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
-
 import eni.fr.BusinessException;
 import eni.fr.bll.ArticleVenduManager;
 import eni.fr.bll.RetraitManager;
 import eni.fr.bo.ArticleVendu;
 import eni.fr.bo.Categorie;
-import eni.fr.bo.Retrait;
 import eni.fr.bo.Utilisateur;
-import eni.fr.dal.ArticleVenduDAOJdbcImpl;
-import eni.fr.dal.DALException;
-import eni.fr.dal.RetraitDAOJdbcImpl;
 
 /**
- * @author ZABAKA FATIMA ZAHRA
- * Servlet implementation class ServletVente
+ * @author ZABAKA FATIMA ZAHRA GAVOILLE FABIEN ALLIOUCHE KERBOUA ABDELREZAK FUCHS EUGENIE
  */
-@MultipartConfig(
-		  fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
-		  maxFileSize = 1024 * 1024 * 10,      // 10 MB
-		  maxRequestSize = 1024 * 1024 * 100) // 100 MB
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+		maxFileSize = 1024 * 1024 * 10, // 10 MB
+		maxRequestSize = 1024 * 1024 * 100) // 100 MB
 @WebServlet("/ServletVente")
 public class ServletVente extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public static final String IMAGES_FOLDER = "C://Users/vmware/Documents/JavaProject/ENI-Encheres/src/main/webapp/img/ArticlesPhotos/";
-	//C://Users/eugen/git/Enchere/src/main/webapp/img/ArticlesPhotos/ Eugenie
+	// C://Users/eugen/git/Enchere/src/main/webapp/img/ArticlesPhotos/ Eugenie
 	// C://Users/Pc/git/Enchere/src/main/webapp/img/ArticlesPhotos/ Fatima
-	// C://Users/vmware/Documents/JavaProject/ENI-Encheres/src/main/webapp/img/ArticlesPhotos/ Abdel
-  
+	// C://Users/vmware/Documents/JavaProject/ENI-Encheres/src/main/webapp/img/ArticlesPhotos/
+	// Abdel
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/Vente.jsp");
-		rd.forward(request, response);	
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Vente.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session=request.getSession();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
 		Utilisateur utilisateurLogged = (Utilisateur) session.getAttribute("utilisateurLogged");
-		
-		//commentaire
+
+		// commentaire
 		String nomArticle;
 		String description;
-	//	Utilisateur noutilisateur=null;
+		// Utilisateur noutilisateur=null;
 		Categorie noCategorie = new Categorie();
-		//photo
+		// photo
 		int miseAPrix;
 		LocalDate dateDebutEncheres;
 		LocalDate dateFinEncheres;
-		String noArticle;
-		//rue
-		//codepostal
-		//ville
+		// rue
+		// codepostal
+		// ville
 		Part filePart = request.getPart("photo");
 		try {
-			nomArticle= request.getParameter("article");
-			description= request.getParameter("description");
-			dateDebutEncheres= LocalDate.parse(request.getParameter("debutenchere"));
-			dateFinEncheres=LocalDate.parse(request.getParameter("finenchere"));
-			miseAPrix= Integer.parseInt(request.getParameter("miseaprix"));			
-				
-			
+			nomArticle = request.getParameter("article");
+			description = request.getParameter("description");
+			dateDebutEncheres = LocalDate.parse(request.getParameter("debutenchere"));
+			dateFinEncheres = LocalDate.parse(request.getParameter("finenchere"));
+			miseAPrix = Integer.parseInt(request.getParameter("miseaprix"));
+
 			int no_categorie = Integer.parseInt(request.getParameter("no_categorie"));
 			noCategorie.setNoCategorie(no_categorie);
-			//int no_utilisateur = Integer.parseInt(request.getParameter("no_utilisateur"));
-			String rue= request.getParameter("rue");
-			String codePostal= request.getParameter("codePostal");
-			String ville= request.getParameter("ville");
+			// int no_utilisateur =
+			// Integer.parseInt(request.getParameter("no_utilisateur"));
+			String rue = request.getParameter("rue");
+			String codePostal = request.getParameter("codePostal");
+			String ville = request.getParameter("ville");
 			ArticleVenduManager articleVenduManager = new ArticleVenduManager();
-			RetraitManager retraitManager=new RetraitManager();
-			
+			RetraitManager retraitManager = new RetraitManager();
+
 //			noArticle= request.getParameter("article");
-			
-			if(nomArticle != null && description != null && miseAPrix != 0 && dateDebutEncheres != null && 
-					dateFinEncheres != null && noCategorie != null && utilisateurLogged != null && rue != null && codePostal != null && ville != null /* && filePart != null*/)
-			
+
+			if (nomArticle != null && description != null && miseAPrix != 0 && dateDebutEncheres != null
+					&& dateFinEncheres != null && noCategorie != null && utilisateurLogged != null && rue != null
+					&& codePostal != null && ville != null /* && filePart != null */)
+
 			{
 //				Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue,
 //						 codePostal, ville, motDePasse, credit, administrateur);
 //				
-				//ArticleVendu art =new ArticleVendu(nomArticle,description,dateDebutEncheres,dateFinEncheres,miseAPrix,noCategorie);
-				//art.setNoUtilisateur(utilisateurLogged);
-			
+				// ArticleVendu art =new
+				// ArticleVendu(nomArticle,description,dateDebutEncheres,dateFinEncheres,miseAPrix,noCategorie);
+				// art.setNoUtilisateur(utilisateurLogged);
 
 				try {
-					ArticleVendu Narticle =articleVenduManager.ajouter(nomArticle, description, dateDebutEncheres, dateFinEncheres, miseAPrix, utilisateurLogged, noCategorie);
+					ArticleVendu Narticle = articleVenduManager.ajouter(nomArticle, description, dateDebutEncheres,
+							dateFinEncheres, miseAPrix, utilisateurLogged, noCategorie);
 					String fileName = Narticle.getNoArticle() + ".jpg";
 					filePart.write(IMAGES_FOLDER + fileName);
 					System.out.println(fileName);
-					//retraitArticle.getNoArticle().setNoArticle(Narticle);
-				//	System.out.println(retraitArticle.getNoArticle().getNoArticle());
-					
-						
-					
-				    
-					
-					Retrait retraitArticle= retraitManager.ajouter(rue, codePostal, ville, Narticle);
-					
-					
-					
+					// retraitArticle.getNoArticle().setNoArticle(Narticle);
+					// System.out.println(retraitArticle.getNoArticle().getNoArticle());
+
+					retraitManager.ajouter(rue, codePostal, ville, Narticle);
+
 				} catch (BusinessException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				//System.out.println(art.getNomArticle()+" "+art.getDescription()+" "+art.getDateDebutEncheres()+" "+art.getDateFinEncheres()+" "+art.miseAPrix+" ");
-				
-			}else if (nomArticle != null && description != null && miseAPrix != 0 && dateDebutEncheres != null && 
-					dateFinEncheres != null && noCategorie != null && utilisateurLogged != null && rue==null && codePostal==null && ville==null /* && filePart != null */) {
-				ArticleVendu art =new ArticleVendu(nomArticle,description,dateDebutEncheres,dateFinEncheres,miseAPrix,noCategorie);
+				// System.out.println(art.getNomArticle()+" "+art.getDescription()+"
+				// "+art.getDateDebutEncheres()+" "+art.getDateFinEncheres()+" "+art.miseAPrix+"
+				// ");
+
+			} else if (nomArticle != null && description != null && miseAPrix != 0 && dateDebutEncheres != null
+					&& dateFinEncheres != null && noCategorie != null && utilisateurLogged != null && rue == null
+					&& codePostal == null && ville == null /* && filePart != null */) {
+				ArticleVendu art = new ArticleVendu(nomArticle, description, dateDebutEncheres, dateFinEncheres,
+						miseAPrix, noCategorie);
 				art.setNoUtilisateur(utilisateurLogged);
-				rue=utilisateurLogged.getRue();
-				codePostal=utilisateurLogged.getCodePostal();
-				ville=utilisateurLogged.getVille();
-				
-				Retrait retraitArticle=new Retrait(rue,codePostal,ville);
+				rue = utilisateurLogged.getRue();
+				codePostal = utilisateurLogged.getCodePostal();
+				ville = utilisateurLogged.getVille();
+
 				try {
-					articleVenduManager.ajouter(nomArticle, description, dateDebutEncheres, dateFinEncheres, miseAPrix, utilisateurLogged, noCategorie);
+					articleVenduManager.ajouter(nomArticle, description, dateDebutEncheres, dateFinEncheres, miseAPrix,
+							utilisateurLogged, noCategorie);
 					retraitManager.ajouter(rue, codePostal, ville, art);
 //					filePart.write(IMAGES_FOLDER + fileName);
-					
+
 				} catch (BusinessException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				System.out.println(art.getNomArticle()+" "+art.getDescription()+" "+art.getDateDebutEncheres()+" "+art.getDateFinEncheres()+" "+art.miseAPrix+" ");
-				
+				System.out.println(art.getNomArticle() + " " + art.getDescription() + " " + art.getDateDebutEncheres()
+						+ " " + art.getDateFinEncheres() + " " + art.miseAPrix + " ");
+
 			}
-			
+
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		}
-	
-		RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/Vente.jsp");
-		rd.forward(request, response);	
-		
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Vente.jsp");
+		rd.forward(request, response);
+
 	}
 
 }
